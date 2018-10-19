5562e82c78068e8ad98e5e6462547f0ea9e7d195
Pierre <generic@diademe.fr>
2018-10-19 12:01:45 +0200

    test: History

---
+++ spec/type.spec.ts
@@ -0,0 +1,70 @@
+import { MasterIndex } from "../src/core";
+import { History } from "../src/type";
+
+describe("type", () => {
+    test("get | set", () => {
+        const m = new MasterIndex();
+        const h = new History(m, 1);
+        expect(h.get()).toBe(1);
+        h.set(0);
+        expect(h.get()).toEqual(0);
+        m.save();
+        expect(h.get()).toEqual(0);
+        expect(m.getCurrentIndex()).toBe(1);
+
+        h.set(1);
+        expect(h.get()).toEqual(1);
+        m.save();
+        expect(m.getCurrentIndex()).toBe(2);
+
+        m.undo();
+        expect(h.get()).toEqual(0);
+
+        h.set(2);
+        expect(h.get()).toEqual(2);
+        m.save();
+        expect(m.getCurrentIndex()).toBe(2);
+
+        h.set(3);
+        expect(h.get()).toEqual(3);
+
+        m.save();
+        expect(m.getCurrentIndex()).toBe(3);
+
+        h.set(4);
+        expect(h.get()).toEqual(4);
+        m.save();
+        expect(m.getCurrentIndex()).toBe(4);
+
+        m.undo();
+
+        h.set(5);
+        expect(h.get()).toBe(5);
+
+        m.save();
+        expect(m.getCurrentIndex()).toBe(4);
+
+        expect(h.get()).toBe(5);
+        m.undo(2);
+        expect(m.getCurrentIndex()).toBe(2);
+
+        h.set(6);
+        expect(h.get()).toBe(6);
+
+        m.save();
+        expect(m.getCurrentIndex()).toBe(3);
+
+        m.undo(0);
+        // h is not defied at index 0 (it is created at index 1)
+        expect(() => h.get()).toThrow();
+
+        m.redo(1);
+        expect(h.get()).toBe(0);
+
+        h.set(7);
+        expect(h.get()).toBe(7);
+
+        m.save();
+        expect(m.getCurrentIndex()).toBe(2);
+    });
+});
