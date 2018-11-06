781bbb63c01b58b5985e9056d1c10fea9487f68b
Pierre <generic@diademe.fr>
2018-11-06 14:07:50 +0100

    test: comment and correction for clone

--- spec/clone.spec.ts
+++ spec/clone.spec.ts
@@ -6,6 +6,7 @@ describe("clone", () => {
     class Foo {
         public array = [1, 2, 3];
         constructor(public name: string) {}
+        // the clone function is inside the class, it is evaluated before the class Foo being warped.
         @cloneClass
         clone(): Foo {
             return new Foo("clone of " + this.name);
@@ -17,6 +18,7 @@ describe("clone", () => {
         public array = [4, 5, 6];
         constructor(public name: string) {}
     }
+    // the clone function is outside the class Bar, so its first argument is a proxy and not the original class
     cloneFunc(Bar, (obj: Bar): Bar => {
         const res = new Bar("clone of " + obj.name);
         res.array = obj.array;
@@ -39,7 +41,8 @@ describe("clone", () => {
 
     test("cloneFunc", () => {
         const bar = new Bar("bar");
-        const barClone = clone.get((bar as any).__proxyInternal__.constructor.originalConstructor, bar);
+        // here, the clone function is outside the class, so when it reference Bar, it is actually the warper of bar that is called
+        const barClone = clone.get(Bar, bar);
         expect(barClone).toBeInstanceOf(Bar);
         expect(barClone.name).toEqual("clone of bar");
         expect(barClone.array).toEqual(bar.array);
@@ -47,7 +50,8 @@ describe("clone", () => {
 
     test("default", () => {
         const baz = new Baz("baz");
-        const bazClone = clone.get((baz as any).__proxyInternal__.constructor.__originalConstructor__, baz);
-        expect(bazClone).toBe(bazClone);
+        // default clone, nothing to worries about
+        const bazClone = clone.get(Baz, baz);
+        expect(bazClone).toEqual(baz);
     });
 });
