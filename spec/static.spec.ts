d4faed2ebeb2d026869955c4ecab95f78fbef6fc
Pierre <generic@diademe.fr>
2018-11-06 13:41:26 +0100

    test: static member

--- spec/static.spec.ts
+++ spec/static.spec.ts
@@ -1,17 +1,49 @@
 import { Undoable, UndoRedo } from "../src/index";
 
 describe("static", () => {
-    @Undoable()
-    class A {
-        static st = 1;
-    }
+    /* Attention, Class history are not reinitialized between test */
+    test("static inheritance", () => {
+        @Undoable()
+        class A {
+            static st = 1;
+        }
 
-    test("static", () => {
-        const ud = new UndoRedo(A);
+        @Undoable()
+        class B extends A {
+            static st1 = 1;
+        }
+        const ud = new UndoRedo();
+        ud.multiAdd([A, B])
+        expect(Object.keys(A)).toEqual(["st"]);
+        expect(Object.keys(B)).toEqual(["st1"]);
+    });
+
+    test("static inheritance", () => {
+        @Undoable()
+        class A {
+            static st = 1;
+        }
+
+        @Undoable()
+        class B extends A {
+            static st = 1;
+        }
+        const ud = new UndoRedo();
+        ud.multiAdd([A, B])
+
+
         A.st = 2;
         expect(A.st).toBe(2);
+        expect(ud.undoPossible()).toBe(true);
         ud.undo();
         expect(A.st).toBe(1);
+
+        B.st = 3;
+        expect(A.st).toBe(1);
+        expect(B.st).toBe(3);
+        ud.undo();
+        expect(A.st).toBe(1);
+        expect(B.st).toBe(1);
     })
 
 });
