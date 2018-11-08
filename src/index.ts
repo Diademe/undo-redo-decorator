a9c9cffb1e20eaec13e93aab7677d72de180f386
Pierre <generic@diademe.fr>
2018-11-08 14:44:30 +0100

    fix: expose Map and Set to the public interface

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { MasterIndex, __initialization__ } from "./core";
 
 export { Undoable } from "./annotation";
+export { Map } from "./collection/map";
+export { Set } from "./collection/set";
 
 export class UndoRedo {
     private index: MasterIndex;
