036a0a385afc428ff80f0a49e9499422e70cf6a4
pierre <pierre.macherel.ext@veolia.com>
2019-05-28 15:22:26 +0200

    test: test the decorator UndoDoNotRecurs

--- spec/decorator.spec.ts
+++ spec/decorator.spec.ts
@@ -1,6 +1,6 @@
 import { Undoable, UndoDoNotTrack, UndoRedo } from "../src/index";
 import { is_constructor } from "../src/utils";
-import { UndoAfterLoad } from "../src/decorator";
+import { UndoAfterLoad, UndoDoNotRecurs } from "../src/decorator";
 
 describe("decorator", () => {
     const db = new Set<any>();
@@ -102,6 +102,43 @@ describe("decorator", () => {
     });
 
     describe("Do not track", () => {
+        test("inheritance", () => {
+            class MotherTest {
+                @UndoDoNotTrack
+                motherDNT: Number;
+                constructor() {
+                    this.motherDNT = 42;
+                }
+            }
+            @Undoable()
+            class ChildTest extends MotherTest {
+                @UndoDoNotTrack
+                childDNT: Number;
+                childDoTrack: Number;
+                constructor() {
+                    super();
+                    this.childDNT = 43;
+                    this.childDoTrack = 18
+                }
+            }
+            const childTest = new ChildTest();
+            const ud = new UndoRedo(childTest);
+            expect(childTest.motherDNT).toEqual(42);
+            expect(childTest.childDNT).toEqual(43);
+            expect(childTest.childDoTrack).toEqual(18);
+            childTest.motherDNT = 15;
+            childTest.childDNT = 44;
+            childTest.childDoTrack = 19;
+            ud.save();
+            expect(childTest.motherDNT).toEqual(15);
+            expect(childTest.childDNT).toEqual(44);
+            expect(childTest.childDoTrack).toEqual(19);
+            ud.undo();
+            expect(childTest.motherDNT).toEqual(15);
+            expect(childTest.childDNT).toEqual(44);
+            expect(childTest.childDoTrack).toEqual(18);
+        });
+
         test("test 1", () => {
             @Undoable()
             class MotherTest {
@@ -159,6 +196,115 @@ describe("decorator", () => {
             expect(childTest.motherDNT).toEqual(15);
             ud.undo();
             expect(childTest.motherDNT).toEqual(15);
+        });
+    });
+
+    describe("Do not recurs", () => {
+        test("inheritance", () => {
+            @Undoable()
+            class T {
+                constructor(public n: number) {}
+            }
+            class MotherTest {
+                @UndoDoNotRecurs
+                motherDNT: T;
+                constructor() {
+                    this.motherDNT = new T(42);
+                }
+            }
+            @Undoable()
+            class ChildTest extends MotherTest {
+                @UndoDoNotRecurs
+                childDNT: T;
+                childDoTrack: T;
+                constructor() {
+                    super();
+                    this.childDNT = new T(43);
+                    this.childDoTrack = new T(18);
+                }
+            }
+            const childTest = new ChildTest();
+            const ud = new UndoRedo(childTest);
+            expect(childTest.motherDNT.n).toEqual(42);
+            expect(childTest.childDNT.n).toEqual(43);
+            expect(childTest.childDoTrack.n).toEqual(18);
+            childTest.motherDNT.n = 15;
+            childTest.childDNT.n = 44;
+            childTest.childDoTrack.n = 19;
+            ud.save();
+            expect(childTest.motherDNT.n).toEqual(15);
+            expect(childTest.childDNT.n).toEqual(44);
+            expect(childTest.childDoTrack.n).toEqual(19);
+            ud.undo();
+            expect(childTest.motherDNT.n).toEqual(15);
+            expect(childTest.childDNT.n).toEqual(44);
+            expect(childTest.childDoTrack.n).toEqual(18);
+        });
+
+        test("test 1", () => {
+            @Undoable()
+            class T {
+                constructor(public n: number) {}
+            }
+            class MotherTest {
+                @UndoDoNotRecurs
+                motherDNT: T;
+                constructor() {
+                    this.motherDNT = new T(42);
+                }
+            }
+            @Undoable()
+            class ChildTest extends MotherTest {
+                @UndoDoNotRecurs
+                childDNT: T;
+                childDoTrack: T;
+                constructor() {
+                    super();
+                    this.childDNT = new T(43);
+                    this.childDoTrack = new T(18);
+                }
+            }
+            const childTest = new ChildTest();
+            const ud = new UndoRedo(childTest);
+            expect(childTest.motherDNT.n).toEqual(42);
+            expect(childTest.childDNT.n).toEqual(43);
+            expect(childTest.childDoTrack.n).toEqual(18);
+            childTest.motherDNT.n = 15;
+            childTest.childDNT.n = 44;
+            childTest.childDoTrack.n = 19;
+            ud.save();
+            expect(childTest.motherDNT.n).toEqual(15);
+            expect(childTest.childDNT.n).toEqual(44);
+            expect(childTest.childDoTrack.n).toEqual(19);
+            ud.undo();
+            expect(childTest.motherDNT.n).toEqual(15);
+            expect(childTest.childDNT.n).toEqual(44);
+            expect(childTest.childDoTrack.n).toEqual(18);
+        });
+
+        test("test 2", () => {
+            @Undoable()
+            class T {
+                constructor(public n: number) {}
+            }
+            @Undoable()
+            class MotherTest {
+                @UndoDoNotTrack
+                motherDNT: T;
+                constructor() {
+                    this.motherDNT = new T(42);
+                }
+            }
+            @Undoable()
+            class ChildTest extends MotherTest {}
+            const childTest = new ChildTest();
+            const ud = new UndoRedo(childTest);
+            expect(childTest.motherDNT.n).toEqual(42);
+            childTest.motherDNT.n = 15;
+            ud.save();
+            expect(childTest.motherDNT.n).toEqual(15);
+            ud.undo();
+            expect(childTest.motherDNT.n).toEqual(15);
         });
     });
 });
